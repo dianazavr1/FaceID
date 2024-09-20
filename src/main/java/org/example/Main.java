45ce4017c7c0fbe99d23d11592abf8c09dc0149e
dianazavr1 <159002661+dianazavr1@users.noreply.github.com>
2024-09-20 15:14:48 +0600

    подключила OpenCV

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.opencv.core.Core;
+
+public class Main {
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        System.out.println("OpenCV успешно подключен!");
+    }
+}
