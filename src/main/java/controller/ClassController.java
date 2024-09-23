a8371175f110fc65d0932036122a6df9c824d138
dianazavr1 <159002661+dianazavr1@users.noreply.github.com>
2024-09-23 22:10:02 +0600

    JavaFX

---
+++ src/main/java/controller/ClassController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javafx.fxml.FXML;
+
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+
+public class ClassController {
+    @FXML
+    private Button button;
+    @FXML
+    private ImageView currentFrame;
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
 import org.opencv.core.Core;
-
+import org.opencv.core.Mat ;
+import org.opencv.core.CvType ;
 public class Main {
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        System.out.println("OpenCV успешно подключен!");
-    }
-}
+        Mat mat = Mat.eye ( 3 , 3 , CvType.CV_8UC1 ) ;
+        System.out.println ( " mat = " + mat.dump ( ) ) ; }}
+
+
+        //  System.out.println("OpenCV успешно подключен!");
+
